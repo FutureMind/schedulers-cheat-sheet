823a6d8fecfb7d7ae4e51645f4c48d416b332157
Michał Klimczak <m.klimczak@futuremind.com>
2017-08-13 00:44:13 +0200

    naming refactoring

--- app/src/test/java/com/futuremind/subscriberscheatsheet/AnimalsMaker.kt
+++ app/src/test/java/com/futuremind/subscriberscheatsheet/AnimalsMaker.kt
@@ -1,8 +1,19 @@
 package com.futuremind.subscriberscheatsheet
 
 interface AnimalsMaker {
+
+    //mammals
     fun makeCat(threadName: String)
     fun makeDog(threadName: String)
+
+    //fish
     fun makeShark(threadName: String)
+
+    //birds
+    fun makeHawk(threadName: String)
+    fun makeSparrow(threadName: String)
+    fun makeMockingbird(threadName: String)
+
+    //reptiles
     fun makeCrocodile(threadName: String)
 }
--- app/src/test/java/com/futuremind/subscriberscheatsheet/SchedulersTest.kt
+++ app/src/test/java/com/futuremind/subscriberscheatsheet/SchedulersTest.kt
@@ -1,5 +1,6 @@
 package com.futuremind.subscriberscheatsheet;
 
+import com.nhaarman.mockito_kotlin.never
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
 import io.reactivex.Completable
@@ -11,12 +12,13 @@ import org.mockito.junit.MockitoJUnitRunner
 
 
 @RunWith(MockitoJUnitRunner::class)
-class SubscribersTest {
+class SchedulersTest {
 
     private val MAMMALS = "mammals scheduler"
     private val REPTILES = "reptiles scheduler"
     private val FISH = "fish scheduler"
-    private val OVERRIDDEN = "this one is never used, it's overridden by mammals scheduler"
+    private val BIRDS = "birds scheduler"
+    private val FLOWERS = " trees scheduler is never used, it's overridden by mammals scheduler"
 
     private val schedulersFactory = NamedSchedulersFactory()
     @Mock lateinit var animalsMaker: AnimalsMaker
@@ -27,8 +29,14 @@ class SubscribersTest {
                 .doOnComplete { animalsMaker.makeDog(getThread()) }
                 .observeOn(scheduler(FISH))
                 .andThen(Completable.fromCallable { animalsMaker.makeShark(getThread()) })
+                .andThen(
+                        Completable.fromCallable { animalsMaker.makeSparrow(getThread()) }
+                                .subscribeOn(scheduler(BIRDS))
+                                .andThen(Completable.fromCallable { animalsMaker.makeHawk(getThread()) })
+                )
+                .andThen(Completable.fromCallable { animalsMaker.makeMockingbird(getThread()) })
                 .subscribeOn(scheduler(MAMMALS))
-                .subscribeOn(scheduler(OVERRIDDEN))
+                .subscribeOn(scheduler(FLOWERS))
                 .observeOn(scheduler(REPTILES))
                 .doOnComplete { animalsMaker.makeCrocodile(getThread()) }
                 .test()
@@ -45,7 +53,27 @@ class SubscribersTest {
     fun `Shark is made on FISH scheduler`() = verify(animalsMaker, times(1)).makeShark(FISH)
 
     @Test
+    fun `Sparrow is made on BIRDS scheduler`() = verify(animalsMaker, times(1)).makeSparrow(BIRDS)
+
+    @Test
+    fun `Hawk is made on BIRDS scheduler`() = verify(animalsMaker, times(1)).makeHawk(BIRDS)
+
+    @Test
+    fun `Mockingbird is made on BIRDS scheduler`() = verify(animalsMaker, times(1)).makeMockingbird(BIRDS)
+
+    @Test
     fun `Crocodile is made on REPTILES scheduler`() = verify(animalsMaker, times(1)).makeCrocodile(REPTILES)
+
+    @Test
+    fun `No animals are made on FLOWERS scheduler`() {
+        verify(animalsMaker, never()).makeDog(FLOWERS)
+        verify(animalsMaker, never()).makeCat(FLOWERS)
+        verify(animalsMaker, never()).makeShark(FLOWERS)
+        verify(animalsMaker, never()).makeSparrow(FLOWERS)
+        verify(animalsMaker, never()).makeHawk(FLOWERS)
+        verify(animalsMaker, never()).makeMockingbird(FLOWERS)
+        verify(animalsMaker, never()).makeCrocodile(FLOWERS)
+    }
 
     private fun getThread() = Thread.currentThread().name
 
