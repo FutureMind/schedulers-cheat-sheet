d7d14720222bfcfce83618fe63193d727415dacf
Michał Klimczak <m.klimczak@futuremind.com>
2017-08-12 20:21:23 +0200

    fighting the completable andThen issue

--- app/src/main/java/com/futuremind/subscriberscheatsheet/SubscribersSample.kt
+++ app/src/main/java/com/futuremind/subscriberscheatsheet/SubscribersSample.kt
@@ -1,6 +1,7 @@
 package com.futuremind.subscriberscheatsheet
 
 import io.reactivex.Completable
+import io.reactivex.Flowable
 
 class SubscribersSample(val thingsMaker: ThingsMaker) {
 
@@ -9,16 +10,31 @@ class SubscribersSample(val thingsMaker: ThingsMaker) {
     companion object {
         val CARS = "cars scheduler"
         val FRUITS = "fruits scheduler"
+        val INSECTS = "insects scheduler"
         val OVERRIDDEN = "this one is never used, it's overridden by fruits scheduler"
     }
 
     fun makeThings(): Completable {
 
-        return Completable.fromCallable { thingsMaker.makeBanana(getThread()) }
-                .doOnComplete { thingsMaker.makePeach(getThread()) }
-                .subscribeOn(scheduler(FRUITS))
-                .subscribeOn(scheduler(OVERRIDDEN))
-                .observeOn(scheduler(CARS))
+//        return Completable.fromCallable { thingsMaker.makeBanana(getThread()) }
+//                .andThen { thingsMaker.makePeach(getThread())  }
+//                .doOnComplete { thingsMaker.makePeach(getThread()) }
+////                .observeOn(scheduler(INSECTS))
+////                .andThen {
+////                    thingsMaker.makeAnt(getThread())
+////                    Completable.complete()
+////                }
+//                .subscribeOn(scheduler(FRUITS))
+////                .subscribeOn(scheduler(OVERRIDDEN))
+//                .observeOn(scheduler(CARS))
+//                .doOnComplete { thingsMaker.makeFerrari(getThread()) }
+
+        return Flowable.fromCallable { thingsMaker.makeBanana(getThread()) }
+                .flatMap { Flowable.fromCallable { thingsMaker.makePeach(getThread()) } }
+                .flatMapCompletable { Completable.complete() }
+                .andThen { Completable.complete() }
+//                .subscribeOn(scheduler(FRUITS))
+//                .observeOn(scheduler(CARS))
                 .doOnComplete { thingsMaker.makeFerrari(getThread()) }
 
     }
@@ -30,6 +46,7 @@ class SubscribersSample(val thingsMaker: ThingsMaker) {
     interface ThingsMaker {
         fun makeBanana(threadName: String)
         fun makePeach(threadName: String)
+        fun makeAnt(threadName: String)
         fun makeFerrari(threadName: String)
     }
 
--- app/src/test/java/com/futuremind/subscriberscheatsheet/SubscribersSampleTest.kt
+++ app/src/test/java/com/futuremind/subscriberscheatsheet/SubscribersSampleTest.kt
@@ -2,11 +2,13 @@ package com.futuremind.subscriberscheatsheet;
 
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
+import io.reactivex.Completable
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.TimeUnit
 
 
 @RunWith(MockitoJUnitRunner::class)
@@ -17,18 +19,30 @@ class SubscribersSampleTest {
 
     @Before
     fun setUp() {
-        subscribersSample = SubscribersSample(listener)
-        val testSubscriber = subscribersSample.makeThings().test()
-        testSubscriber.awaitTerminalEvent()
+//        subscribersSample = SubscribersSample(listener)
+//        val testSubscriber = subscribersSample.makeThings().test()
+//        testSubscriber.awaitTerminalEvent(4, TimeUnit.SECONDS)
     }
 
     @Test
-    fun `Peach is made on FRUITS scheduler`() = verify(listener, times(1)).makePeach(SubscribersSample.FRUITS)
+    fun `Completable wtf`(){
+        Completable.complete()
+                .andThen{ Completable.complete() }
+                .test()
+                .assertComplete()
+//                .awaitTerminalEvent()
+    }
 
-    @Test
-    fun `Banana is made on FRUITS scheduler`() = verify(listener, times(1)).makeBanana(SubscribersSample.FRUITS)
-
-    @Test
-    fun `Ferrari is made on CARS scheduler`() = verify(listener, times(1)).makeFerrari(SubscribersSample.CARS)
+//    @Test
+//    fun `Peach is made on FRUITS scheduler`() = verify(listener, times(1)).makePeach(SubscribersSample.FRUITS)
+//
+//    @Test
+//    fun `Banana is made on FRUITS scheduler`() = verify(listener, times(1)).makeBanana(SubscribersSample.FRUITS)
+//
+//    @Test
+//    fun `Ant is made on INSECTS scheduler`() = verify(listener, times(1)).makeAnt(SubscribersSample.FRUITS)
+//
+//    @Test
+//    fun `Ferrari is made on CARS scheduler`() = verify(listener, times(1)).makeFerrari(SubscribersSample.CARS)
 
 }
